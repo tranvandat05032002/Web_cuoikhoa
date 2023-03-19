425ecc291351a47549a98573210ee2562b775467
Spider Man <tranvandathtml0503@gmail.com>
2023-03-20 03:42:19 +0700

    layout dashboard

---
+++ src/components/icons/iconDown.js
@@ -0,0 +1,21 @@
+import classNames from "../../utils/classNames";
+
+const IconDown = ({ className = "" }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      stroke="currentColor"
+      className={classNames("w-6 h-6", className)}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+      />
+    </svg>
+  );
+};
+export default IconDown;
--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,4 +1,26 @@
 import IconCheck from "./iconCheck";
+import IconDashboard from "./iconDashboard";
+import IconDown from "./iconDown";
 import IconEyeToggle from "./IconEyeToggle";
-
-export { IconCheck, IconEyeToggle };
+import IconLogout from "./iconLogout";
+import IconPayment from "./iconPayment";
+import IconProfile from "./iconProfile";
+import IconSearch from "./iconSearch";
+import IconUpTrending from "./iconUpTrending";
+import IconWithdraw from "./iconWithdraw";
+import IconCampaign from "./iconCampaign"
+import IconLightDark from "./iconLightDark";
+export {
+  IconCheck,
+  IconEyeToggle,
+  IconSearch,
+  IconDown,
+  IconUpTrending,
+  IconDashboard,
+  IconLogout,
+  IconProfile,
+  IconPayment,
+  IconWithdraw,
+  IconCampaign,
+  IconLightDark
+};
