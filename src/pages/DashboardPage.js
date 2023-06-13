caba395fb459112531641c08dfe646c0876abd8a
Spider Man <tranvandathtml0503@gmail.com>
2023-06-13 21:31:34 +0700

    use interceptor get data

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -5,7 +5,21 @@ import CampaignItem from "../modules/campaign/CampaignItem";
 import { v4 as uuidV4 } from "uuid";
 import Gap30 from "../components/common/Gap30";
 import CampaignFeature from "../modules/campaign/CampaignFeature";
+import useAxiosPrivate from "../hooks/useAxiosPrivate";
 const DashboardPage = () => {
+  const axiosPrivate = useAxiosPrivate();
+  React.useEffect(() => {
+    async function fetchCampaigns() {
+      try {
+        const response = await axiosPrivate.get("/api/campaigns");
+        console.log(response);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchCampaigns();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <React.Fragment>
       <Heading number="4">Your Campaign</Heading>
